d6720deebe4cc72654ebcb32d2f5730e987b5af8
tajdingurdal <tjdngrdl1@gmail.com>
2022-07-17 18:50:44 +0300

    calismaya devamke

--- src/main/java/com/gokhantamkoc/javabootcamp/odevhafta45/repository/ProductRepository.java
+++ src/main/java/com/gokhantamkoc/javabootcamp/odevhafta45/repository/ProductRepository.java
@@ -32,7 +32,7 @@ public class ProductRepository {
 	public List<Product> getAll() {
 
 		// BU METHODU 1. GOREV ICIN DOLDURUNUZ
-		final String SQL = "SELECT id, name, description FROM public.product";
+		final String SQL = "SELECT * FROM public.product";
 		List<Product> products = new ArrayList<>();
 
 		try (PreparedStatement preparedStatement = databaseConnection.getConnection().prepareStatement(SQL)) {
